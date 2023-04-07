30d4aa1a608fb7d28446701761ee36a9f60b0e91
luizsouzars <luiz.souza94@edu.pucrs.br>
2023-04-07 01:54:15 -0300

    tests

--- src/Renner_lastmile.py
+++ src/Renner_lastmile.py
@@ -5,7 +5,7 @@ import numpy as np
 dataframe = pd.read_csv(r'data/dados_entregas_last_mile.csv',sep=';')
 
 st.title("___ Renner - LastMile ___")
-st.subtitle("Projeto da disciplina de Visualização de Dados\nProfessora: Isabel")
+st.subheader("Projeto da disciplina de Visualização de Dados\nProfessora: Isabel")
 st.write("Desenvolvido por:\nFlorensa Dimer, Leandra Torbes e Luiz Eduardo de Souza")
 
 # st.dataframe(dataframe.style.highlight_max(axis=0))
