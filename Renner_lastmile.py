07638488066b62556bc0e409d27a6a216dd54279
luizsouzars <luiz.souza94@edu.pucrs.br>
2023-04-07 01:17:17 -0300

    tests

--- Renner_lastmile.py
+++ Renner_lastmile.py
@@ -4,5 +4,9 @@ import numpy as np
 
 dataframe = pd.read_csv('dados_entregas_last_mile.csv',sep=';')
 
+st.write("Renner")
+st.write("LastMile\n")
+st.write("Luiz Eduardo de Souza")
+
 # st.dataframe(dataframe.style.highlight_max(axis=0))
 st.dataframe(dataframe)
