685ab0ae4fd445fb19a9663fce0cde5704664dd6
luizsouzars <luiz.souza94@edu.pucrs.br>
2023-04-07 01:36:59 -0300

    tests

---
+++ src/Renner_lastmile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+dataframe = pd.read_csv('data\dados_entregas_last_mile.csv',sep=';')
+
+st.title("Renner - LastMile")
+st.subtitle("Projeto da disciplina de Visualização de Dados\nProfessora: Isabel")
+st.write("Desenvolvido por:\nFlorensa Dimer, Leandra Torbes e Luiz Eduardo de Souza")
+
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+st.dataframe(dataframe.head())
