b3fc0c610c07204c1fa41955d4ce384b8f11f387
luizsouzars <luiz.souza94@edu.pucrs.br>
2023-04-07 00:42:56 -0300

    First Steamlit test

---
+++ Renner_lastmile.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+dataframe = pd.read_csv('dados_entregas_last_mile.csv',sep=';')
+
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+st.dataframe()
